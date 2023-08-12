f95e63c2696399658319c2d27cc10289cf2ba5e3
Liis Lille <liislille@gmail.com>
2023-08-12 13:16:21 +0300

    Day 1 project - Band Name Generator

---
+++ Data types.py
@@ -0,0 +1,14 @@
+#Data types
+
+print("Hello"[1]) #Subscripting
+
+# 🚨 Don't change the code below 👇
+height = float(input("enter your height in m: "))
+weight = float(input("enter your weight in kg: "))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+bmi = weight/height**2
+print(int(bmi))
+
+print(round(3/2, 2))
